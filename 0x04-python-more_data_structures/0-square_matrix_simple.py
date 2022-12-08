e86b5bb8fc5bbbcc38d5c49b5bdd37f73dc997a1
Eugene Kartey <eugene_1@live.co.uk>
2022-12-08 12:51:19 +0000

    sqrd matrix done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    sqr_matrix = []
-    for mtrx_row in matrix:
-        mtrx_row = list(map(lambda x: x**2, row))
-        sqr_mtx.append(mtrx_row)
-    return sqr_mtx
+    """
+    Compute the square value of all integers of a matrix
+    """
+    return ([[(x**2) for x in row] for row in matrix])
